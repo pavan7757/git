2c7e66ed433fc338e7d40ef3fc42ce3ce3f07a02
pavan7757 <patelpavan7757@gmail.com>
2024-08-06 13:34:11 +0530

    first commit daw daw

--- asdkjnf.java
+++ asdkjnf.java
@@ -1,5 +1,11 @@
 public class asdkjnf {
     public static void main(String[] args) {
         System.out.println("daw daw daw");
+        System.out.println("daw daw daw");
+        System.out.println("daw daw daw");
+        System.out.println("daw daw daw");
+        System.out.println("daw daw daw");
+        System.out.println("daw daw daw");
+
     }
 }
