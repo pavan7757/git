aee94de6cd09c9bc2d1c4dc4c22b7d38b1935481
pavan7757 <patelpavan7757@gmail.com>
2024-08-10 14:46:27 +0530

    first comit temprature

---
+++ question.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class question {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        double f = Sc.nextInt();
+        System.out.println(f *9/5 + (32));
+
+    }
+}
+
+
+
