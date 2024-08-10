061ef12c12499b760c9a27d5404b880d24296692
pavan7757 <patelpavan7757@gmail.com>
2024-08-10 15:42:10 +0530

    first commit even or odd

---
+++ que1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class que1{
+    public static void main(String [] args){
+        Scanner Sc = new Scanner (System.in);
+        int a=Sc.nextInt();
+        if (a%2==0){
+            System.out.println(" number id even");
+        } else{
+            System.out.println("number is odd");
+        }
+
+    }
+}
