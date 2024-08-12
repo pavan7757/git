da5ffc23e36ed048fa8358616ad1c66ab72db9c5
pavan7757 <patelpavan7757@gmail.com>
2024-08-12 23:28:37 +0530

    mutiple(table of any number)

---
+++ table.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[]arg){
+        Scanner Sc=new Scanner(System.in);
+        int a= Sc.nextInt();
+        for(int i=1;i<=10; i++){
+            System.out.println(i*a);
+
+
+
+
+        }
+
+
+
+    }
+}
+
+
