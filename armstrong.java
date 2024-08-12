af86987de12d82576ea69dd146116e2360e81c1d
pavan7757 <patelpavan7757@gmail.com>
2024-08-12 23:48:51 +0530

    reversed number

--- armstrong.java
+++ armstrong.java
@@ -1,4 +1,4 @@
-public class loop
+public class armstrong
 {
     public static void main(String[] args)
     {
@@ -24,4 +24,3 @@ public class loop
         }
     }
 }
-
--- module-info.java
+++ module-info.java
@@ -1,2 +1,3 @@
 module git {
+    requires kotlin.stdlib;
 }
