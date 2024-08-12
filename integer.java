cf4cbe6531c6325472074f5ccbea558e3e4708bc
pavan7757 <patelpavan7757@gmail.com>
2024-08-12 23:23:04 +0530

    sum of first 10 integer

---
+++ integer.java
@@ -0,0 +1,16 @@
+
+public class integer {
+    public static void main(String[]arg){
+        int sum=0;
+        for(int i=1;i<=10; i++){
+            sum=sum+i;
+
+
+
+        }
+        System.out.println(sum);
+
+
+
+    }
+}
