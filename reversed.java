49ad89c1ebef9480250ab6b4a0948af00ecb080a
pavan7757 <patelpavan7757@gmail.com>
2024-08-12 23:48:51 +0530

    reversed number

---
+++ reversed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class reversed {
+    public static void main(String[]arg){
+        Scanner Sc=new Scanner(System.in);
+        int a= Sc.nextInt();
+        while(a!=0){
+            int sum=a%10;
+            System.out.print(sum);
+            a=a/10;
+
+
+
+
+
+        }
+
+
+
+    }
+}
