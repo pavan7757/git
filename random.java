1a4e2038742134ce5bc4bf13507a188a20688fdc
pavan7757 <patelpavan7757@gmail.com>
2024-08-13 23:56:02 +0530

    game type

---
+++ random.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class random {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.err.println("enter number");
+        int a=Sc.nextInt();
+        System.out.println(a);
+        int randomNum = (int)(Math.random() * 101);  // 0 to 100
+        System.err.println("randaom number is");
+        System.out.println(randomNum);
+
+
+        if (a>randomNum) {
+            System.out.println("Too high");
+            System.err.println("Try again");
+
+        } else if(a<randomNum) {
+            System.out.println("Too low");
+            System.err.println("Try again");
+
+
+        }
+
+    }
+}
+
+
+
+
