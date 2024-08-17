e73db4517cd8b8750bb79ebb5737dc0e8e914a1a
pavan7757 <patelpavan7757@gmail.com>
2024-08-17 21:49:45 +0530

    tringle of number

--- numbertringle.java
+++ numbertringle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class numbertringle{
+public class numbertringle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
