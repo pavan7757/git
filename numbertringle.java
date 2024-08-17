51f6a661f33d167dd1825707b8dfb97dd5185878
pavan7757 <patelpavan7757@gmail.com>
2024-08-17 21:25:36 +0530

    tringle of number

---
+++ numbertringle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class numbertringle{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows for the triangle: ");
+        int n = scanner.nextInt();
+
+        // Generate the triangle of numbers
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
